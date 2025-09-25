2032d0c5bd48f9f1d36726858b8e154c5b97beee
Michal Bazynski <michal@bazynski.pl>
2025-09-25 13:07:53 +0200

    save button in review screen

--- Sources/WeScan/Review/ReviewViewController.swift
+++ Sources/WeScan/Review/ReviewViewController.swift
@@ -47,7 +47,7 @@ final class ReviewViewController: UIViewController {
     }()
 
     private lazy var doneButton: UIBarButtonItem = {
-        let button = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(finishScan))
+        let button = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(finishScan))
         button.tintColor = navigationController?.navigationBar.tintColor
         return button
     }()
